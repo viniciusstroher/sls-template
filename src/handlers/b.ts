95524ebed566e8248d4d5ef8220ef04e5c18425c
Vinicius Stroher <vinicius.stroher@saipos.com>
2021-05-30 23:03:32 -0300

    ajuste

--- src/handlers/b.ts
+++ src/handlers/b.ts
@@ -2,6 +2,11 @@ import 'source-map-support/register';
 
 export const handler = (
     event: any,
+    context: any, 
+    callback: any
 ) => {
     console.log(event);
+    //precisa retornar o callback
+    callback();
+
 }
