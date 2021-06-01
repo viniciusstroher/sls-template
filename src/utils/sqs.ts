76fded421f434482755c4621965357dca66b73f5
Vinicius Stroher <vinicius.stroher@saipos.com>
2021-05-31 22:40:17 -0300

    remocao de pacotes

--- src/utils/sqs.ts
+++ src/utils/sqs.ts
@@ -29,12 +29,12 @@ export const uuidv4 = () => {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
-    });
+    }); 
 }
 
 export const SQSAws = async () => {
     return new Promise(async function(resolve, reject) {
-        try {
+        try { 
 
             AWSCredentials()
             console.log(AWS.config)
